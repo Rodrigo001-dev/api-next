93f0990a660c2420b1c119fc9033be39523e3099
Rodrigo <rodrigorael53@gmail.com>
2022-07-13 16:50:24 -0300

    creating a route to list all users

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,5 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "../../../../node_modules/next/dist/shared/lib/utils";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ ok: true });
+  return res.status(200).json({
+    data: [
+      { id: 1, name: 'Rodrigo' },
+      { id: 2, name: 'Rodolfo' },
+      { id: 3, name: 'Gabriel' },
+    ]
+  });
 };
