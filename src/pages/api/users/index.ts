49a8c0de77bc9cb7ef2fcb36faa20ed1c6837c28
Rodrigo <rodrigorael53@gmail.com>
2022-07-13 18:16:18 -0300

    creating a post and get method in the users route

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,11 +1,30 @@
+import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from "../../../../node_modules/next/dist/shared/lib/utils";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({
-    data: [
-      { id: 1, name: 'Rodrigo' },
-      { id: 2, name: 'Rodolfo' },
-      { id: 3, name: 'Gabriel' },
-    ]
-  });
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+
+  if (method === "GET") {
+    const users = await prisma.user.findMany();
+
+    return res.status(200).json({
+      data: users,
+    });
+  } else if (method === "POST") {
+    const { name } = req.body;
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+      }
+    });
+
+    return res.status(201).json({
+      data: user,
+    });
+  }
+
+  return res.status(404).json({ message: 'Route not found.' });
 };
